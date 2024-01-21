f25b0751ec2947cd4b46fd88a719341fdc81d8cd
Pranay Shanigarapu <47024307+rajpranay06@users.noreply.github.com>
2024-01-21 14:34:16 +0530

    Update MooresVotingAlgorithm.py

--- Arrays/Medium/Majority Element > N by 2 times/MooresVotingAlgorithm.py
+++ Arrays/Medium/Majority Element > N by 2 times/MooresVotingAlgorithm.py
@@ -12,6 +12,17 @@ Example 2:
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2
 
+
+Approach: 
+    1. Initialize 2 variables:
+        Count –  for tracking the count of element
+        Element – for which element we are counting
+    2. Traverse through the given array.
+        If Count is 0 then store the current element of the array as Element.
+        If the current element and Element are the same increase the Count by 1.
+        If they are different decrease the Count by 1.
+    3. The integer present in Element should be the result we are expecting 
+    
 '''
 
 class Solution:
