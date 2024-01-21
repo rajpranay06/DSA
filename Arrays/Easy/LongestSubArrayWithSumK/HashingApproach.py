7e84227bca6efe54a4d16ad684ef11975dd59278
Pranay Shanigarapu <47024307+rajpranay06@users.noreply.github.com>
2024-01-21 13:48:32 +0530

    Create HashingApproach.py for LongestSubArrayWithSumK

---
+++ Arrays/Easy/LongestSubArrayWithSumK/HashingApproach.py
@@ -0,0 +1,42 @@
+def getLongestSubarray(a: [int], k: int) -> int:
+    n = len(a) # size of the array.
+
+    preSumMap = {}
+    Sum = 0
+    maxLen = 0
+    for i in range(n):
+        # calculate the prefix sum till index i:
+        Sum += a[i]
+
+        # if the sum = k, update the maxLen:
+        if Sum == k:
+            maxLen = max(maxLen, i + 1)
+
+        # calculate the sum of remaining part i.e. x-k:
+        rem = Sum - k
+
+        # Calculate the length and update maxLen:
+        if rem in preSumMap:
+            length = i - preSumMap[rem]
+            maxLen = max(maxLen, length)
+
+        # Finally, update the map checking the conditions:
+        if Sum not in preSumMap:
+            preSumMap[Sum] = i
+
+    return maxLen
+
+if __name__ == "__main__":
+	a = [2, 3, 5, 1, 9]
+	k = 10
+
+	length = getLongestSubarray(a, k)
+	print(f"The length of the longest subarray is: {length}")
+
+'''
+Time Complexity: O(N) or O(N*logN) depending on which map data structure we are using, where N = size of the array.
+Reason: For example, if we are using an unordered_map data structure in C++ the time complexity will be O(N)(though in the worst case, unordered_map takes O(N) to find an element and the time complexity becomes O(N2)) but if we are using a map data structure, the time complexity will be O(N*logN). The least complexity will be O(N) as we are using a loop to traverse the array.
+
+Space Complexity: O(N) as we are using a map data structure.
+
+'''
