f9796931a9b10e00b1feae4393bcbade7082b382
Pranay Shanigarapu <47024307+rajpranay06@users.noreply.github.com>
2024-02-27 21:12:53 +0530

    Update SubStringsContainingAllThreeCharacters.py

--- Sliding Window & Two Pointer Combined Problems/Medium Problems/SubStringsContainingAllThreeCharacters.py
+++ Sliding Window & Two Pointer Combined Problems/Medium Problems/SubStringsContainingAllThreeCharacters.py
@@ -40,3 +40,46 @@ class Solution:
                 left += 1
         
         return res
+
+'''
+
+Two Pointers
+
+We will be maintaining three pointers each for characters ‘a’, ‘b’ and ‘c’ i.e ‘aIdx’, ‘bIdx’ and ‘cIdx’ initially initialised to -1 for maintaining the last index of their occurrence.
+We will be iterating via ‘i’ from 0 to ‘n’-1. For each ‘i’ we will update the counters depending on the s[i].
+Once we update the counters, we have to find the last index we essentially have to include to make a valid substring ending at ‘i’. 
+All the substrings before that last index and ending at ‘i’ would be valid.
+The last index is nothing but the minimum of all ‘aIdx’, ‘bIdx’ and ‘cIdx’. We will add all the valid substrings ending at ‘i’ to the final answer.
+
+Time Complexity - O(n),  where ‘n’ is the size of the string ’s'.
+We are iterating via ‘i’ from 0 to ‘n’-1. Hence the total time complexity for the solution is O(1).
+
+Space Complexity - O(1).
+We are not utilising any extra space here, hence the O(1) space complexity.
+
+'''
+
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        ans = 0
+        n = len(s)
+
+        # Initialising index of last occurrence of each character to '-1'.
+        aIdx, bIdx, cIdx = -1, -1, -1
+
+        for i in range(n):
+
+            # Updating the index of last occurrence of each character.
+            if s[i] == 'a':
+                aIdx = i
+            elif s[i] == 'b':
+                bIdx = i
+            else:
+                cIdx = i
+
+            minIdx = min(aIdx, bIdx, cIdx)
+
+            # Adding count of all valid substrings.
+            ans = ans + (minIdx + 1)
+
+        return ans
